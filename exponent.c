c3943659c781d46d008983650b52fe82a266a108
Tanishachopade <tanishachopade@gmail.com>
2024-09-16 21:59:09 +0530

    Add files via upload

---
+++ exponent.c
@@ -0,0 +1,7 @@
+#include <math.h>
+int main(){
+int a;
+a=pow (3,2);
+printf("%d",a);
+return 0;
+}
---
+++ floatmultiply.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+float a,b,product;
+printf("Enter two floating numbers:");
+scanf("%f%f",&a,&b);
+product=a*b;
+printf("product:%f\n",product);
+return 0;
+}
